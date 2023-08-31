cdc8507e1b70e93ebc5413ff2ce2897409e585cb
indev <InDevRus@hotmail.com>
2023-08-31 08:15:35 +0000

    Fixed lesson5 solution

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 select * from payment payment where payment.amount >= 500;
 select * from student student where student.birthday <= dateadd('YEAR', -20, CURRENT_DATE);
 select * from student student where student.groupnumber = 10 and student.birthday > dateadd('YEAR', -20, CURRENT_DATE);
-select * from student student where student.name = 'Mike' or (student.groupnumber = 4 or student.groupnumber = 5 or student.groupnumber = 6);
-select * from payment payment where payment.payment_date <= dateadd('MONTH', -8, CURRENT_DATE);
+select * from student student where locate('Mike', STUDENT.NAME) = 1 or (student.groupnumber = 4 or student.groupnumber = 5 or student.groupnumber = 6);
+select * from payment payment where payment.payment_date >= dateadd('MONTH', -8, CURRENT_DATE);
 select * from student student where substring(student.name from 0 for 1) = 'A';
-select * from student student where (student.name = 'Roxi' and student.groupnumber = 4) or (student.name = 'Tallie' and student.groupnumber = 9);
+select * from student student where (locate('Roxi', student.name) = 1 and student.groupnumber = 4) or (locate('Tallie', student.name) = 1 and student.groupnumber = 9);
