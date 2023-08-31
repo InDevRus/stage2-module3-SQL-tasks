fdcc6fa29b8f66973acd791b98809b7b25d64d56
indev <InDevRus@hotmail.com>
2023-08-31 06:23:50 +0000

    Fixed soltion to Lesson4

--- lesson4/src/main/resources/SOLUTION.sql
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -1,12 +1,4 @@
-select *
-from "Student";
-
-select *
-from "Student"
-limit 50;
-
-select "name"
-from "Student";
-
-select distinct "amount"
-from "Payment";
+select * from "Student";
+select * from "Student" limit 50;
+select "name" from "Student";
+select distinct "amount" from "Payment";
