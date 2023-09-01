d60ae5dabcaaba972365c1a63706a6401616ef84
indev <InDevRus@hotmail.com>
2023-09-01 02:26:16 +0000

    Reformatted solution for lesson12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,3 +1,4 @@
-delete from payment where exists(select * from paymenttype payment_type where payment_type.name = 'DAILY' and payment.type_id = payment_type.id);
+delete from student where student.groupnumber > 4;
+delete from student where exists(select * from mark mark where mark.student_id = student.id and mark.mark <= 4);
 delete from paymenttype where paymenttype.name = 'DAILY';
 delete from mark where mark.mark < 7;
